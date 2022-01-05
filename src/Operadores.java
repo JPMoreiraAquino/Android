4f65ed0e15ed26fa63c0559edc520b284ed76e7f
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-05 20:22:47 -0300

    Operadores em Java

---
+++ src/Operadores.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Operadores {
+    public static void main(String[] args) {
+        Integer numero = 11;
+
+        System.out.println(numero + 990);
+        System.out.println(numero - 110);
+        System.out.println(numero * 10);
+        System.out.println(numero / 2);
+        System.out.println(numero % 2);
+
+
+    }
+}
