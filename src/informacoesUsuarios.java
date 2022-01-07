00c4866ed1e70f70629647fff433e42733a46646
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-07 15:06:07 -0300

    Informações com usuário

---
+++ src/informacoesUsuarios.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class informacoesUsuarios {
+    public static void main(String[] args) {
+
+        Integer valor;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+
+        valor = scanner.nextInt();
+
+        System.out.println(valor + 10);
+
+    }
+}
