65d485491b3e08e86b0da84eb7fe999c10f725d2
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-07 15:19:47 -0300

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,22 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        String nome = "Java";
+
+        System.out.println("Primeiro caractere: " + nome.charAt(0));
+
+        String sobrenome = "Rules!";
+        System.out.println(nome + " " + sobrenome );
+
+        Integer idade = 10;
+        System.out.println("idade: " + idade);
+
+        Double d = 1232324.0;
+
+       String.valueOf(12f);
+
+        String srtdouble = String.valueOf(d);
+
+        System.out.println(String.valueOf(12f));// add 12f
+    }
+}
