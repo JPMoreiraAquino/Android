4182d5e633051cf447c61d8d933475e214283db6
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-07 23:06:51 -0300

    Funções úteis de String

---
+++ src/func.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class func {
+    public static void main(String[] args) {
+        String str = "Java course";
+
+        // System.out.println(str.charAt(10));
+
+        // System.out.println(str.equals("Java course"));
+
+        // System.out.println(str.startsWith("Java"));
+
+        // System.out.println(str.endsWith("course"));
+
+        // System.out.println(str.substring(3, 6));
+        // System.out.println(str.substring(3));
+
+        // System.out.println(str.replace("Java", "Joao Pedro"));
+
+        // System.out.println(str.toUpperCase());
+        // System.out.println(str.toLowerCase());
+
+        // System.out.println(str.trim());
+
+        // System.out.println(str.length());
+
+    }
+}
