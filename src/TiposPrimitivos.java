31901dfef94b8fa8d6b74b980f33d78e80b51d35
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2022-01-07 21:40:43 -0300

    Primitive type

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int a1 = 10;
+
+        Integer a2 = 10;
+
+        /*
+         In Integer has method
+         For data storage
+
+         * Double      - double
+         * Float       - float
+         * Long        - long
+         * Integer     - int
+         * Short       - short
+         * Byte        - byte
+         * Boolean     - boolean
+         * Character   - char
+
+         String has no primitive type
+
+         */
+
+
+
+    }
+}
